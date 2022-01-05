41355ea19be747faa9455967e435824fc12601a4
Jorge Mendez <mendezjorge98@gmail.com>
2022-01-05 01:26:54 -0300

    Agrego archivos de prueba, para subir al repositorio

---
+++ js/practica.js
@@ -0,0 +1,9 @@
+function escribir(){
+    for(let i=0 ;i<=10;i++){
+        document.write("<h1 style='color:red';> HOLA MUNDO</h2>");
+        if(i==5){
+            break;
+        }
+    }
+}
+escribir();
