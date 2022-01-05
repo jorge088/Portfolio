1b14ccbb2aa70e051049860323c3ea4d515591ed
Jorge Mendez <mendezjorge98@gmail.com>
2022-01-05 13:07:09 -0300

    Practica de js y branch

--- js/practica.js
+++ js/practica.js
@@ -7,3 +7,13 @@
 //     }
 // }
 // escribir();
+
+var prese = document.createElement("p");
+var texto = document.createTextNode("NUEVO TEXTO");
+prese.appendChild(texto);
+prese.setAttribute("style","color:red;font-size:40px;text-align:center;")
+
+var padre = document.getElementById('presentacion').parentNode;
+padre.appendChild(prese);
+document.write(prese.innerHTML);
+console.log(prese.innerHTML);
