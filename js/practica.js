c8ea677c0b1995ff48ff0392139eb26e8091b48c
Jorge Mendez <mendezjorge98@gmail.com>
2022-01-23 14:21:53 -0300

    Agrego animaciones a skills

--- js/practica.js
+++ js/practica.js
@@ -22,7 +22,29 @@ window.addEventListener('scroll',function(){
         let alturaAnimado = animado[i].offsetTop;
         if(alturaAnimado - 450 < scrollTop){
             animado[i].style.opacity = 1;
-            animado[i].classList.add("mostrarDerecha")
+            animado[i].classList.add("mostrarDerecha");
+        }
+    }
+});
+let animado2=document.querySelectorAll(".animado2");
+window.addEventListener('scroll',function(){
+    let scrollTop = document.documentElement.scrollTop;
+    for(let i=0;i<animado2.length;i++){
+        let alturaAnimado = animado2[i].offsetTop;
+        if(alturaAnimado - 450 < scrollTop){
+            animado2[i].style.opacity = 1;
+            animado2[i].classList.add("mostrarArriba");
+        }
+    }
+});
+let animado3=document.querySelectorAll(".animado3");
+window.addEventListener('scroll',function(){
+    let scrollTop = document.documentElement.scrollTop;
+    for(let i=0;i<animado3.length;i++){
+        let alturaAnimado = animado3[i].offsetTop;
+        if(alturaAnimado - 450 < scrollTop){
+            animado3[i].style.opacity = 1;
+            animado3[i].classList.add("mostrarAbajo");
         }
     }
 });
